ad226a769e3e55788a7c11953e81e43d97310b0e
vovaberdi <vova.berdibek@gmail.com>
2023-01-25 19:54:47 +0200

    add delete and edit functionality

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -80,7 +80,22 @@ export default function Profile() {
       setLoading(false);
     }
     fetchUserListing();
-  },[auth.currentUser.uid])
+  },[auth.currentUser.uid]);
+
+  async function onDelete(listingId){
+    if(window.confirm("Are you sure you want to delete?")){
+      await deleteDoc(doc(db, "listings", listingId));
+      const updatedListings = listings.filter(
+        (listing)=> listing.id !== listingId
+      );
+      setListings(updatedListings);
+      toast.success("successfully deleted listing");
+    }
+
+  }
+  function onEdit(listingId){
+    navigate(`/edit-listing/${listingId}`)
+  }
   return (
     <>
       <section className='max-w-6xl mx-auto flex justify-center items-center flex-col'>
@@ -144,7 +159,13 @@ export default function Profile() {
           <h2 className='text-2xl text-center font-semibold mb-6'>My Listings</h2>
           <ul className='sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 mt-6 mb-6'>
             {listings.map((listing)=>(
-              <ListingItem key={listing.id} id={listing.id} listing={listing.data}/>
+              <ListingItem 
+              key={listing.id} 
+              id={listing.id} 
+              listing={listing.data}
+              onDelete={()=>onDelete(listing.id)}
+              onEdit={()=>onEdit(listing.id)}
+              />
             ))}
           </ul>
           </>
