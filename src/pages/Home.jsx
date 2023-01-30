4157998f5855e11ce3090c3e7415611bd5379442
vovaberdi <vova.berdibek@gmail.com>
2023-01-30 13:24:38 +0200

    complate home page adding pleaces to rent and sale

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,6 +11,8 @@ import { useEffect, useState } from 'react';
 import Slider from '../components/Slider'
 import Spinner from "../components/Spinner";
 import { db } from "../firebase";
+import { Link } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 
 export default function Home() {
  // Offers
@@ -73,7 +75,7 @@ export default function Home() {
    }
    fetchListings();
  }, []);
- // Places for rent
+ // Places for sale
  const [saleListings, setSaleListings] = useState(null);
  useEffect(() => {
    async function fetchListings() {
@@ -106,6 +108,50 @@ export default function Home() {
   return (
     <div>
       <Slider/>
+      <div className="max-w-6xl mx-auto pt-4 space-y-6">
+        {offerListings && offerListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">Recent offers</h2>
+            <Link to="/offers">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 
+              transition duration-150 ease-in-out">Show more offers</p>
+            </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {offerListings.map((listing)=>(
+                <ListingItem key={listing.id} listing={listing.data} id={listing.id}/>
+              ))}
+            </ul>
+          </div>
+        )}
+         {rentListings && rentListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">Places for rent</h2>
+            <Link to="/category/rent">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 
+              transition duration-150 ease-in-out">Show more places for rent</p>
+            </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {rentListings.map((listing)=>(
+                <ListingItem key={listing.id} listing={listing.data} id={listing.id}/>
+              ))}
+            </ul>
+          </div>
+        )}
+         {saleListings && saleListings.length > 0 && (
+          <div className="m-2 mb-6">
+            <h2 className="px-3 text-2xl mt-6 font-semibold">Places for sale</h2>
+            <Link to="/offers">
+              <p className="px-3 text-sm text-blue-600 hover:text-blue-800 
+              transition duration-150 ease-in-out">Show more places to sale</p>
+            </Link>
+            <ul className="sm:grid sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+              {saleListings.map((listing)=>(
+                <ListingItem key={listing.id} listing={listing.data} id={listing.id}/>
+              ))}
+            </ul>
+          </div>
+        )}
+      </div>
     </div>
   )
 }
